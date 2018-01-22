0a30ba14807c026098d8720bb630e347b5cb4681
Sanjana679 <sanjana@nandi.net>
2018-01-22 15:26:06 -0800

    Added more to the RobotMap

--- 2018-Code/src/org/usfirst/frc/team2643/robot/Robot.java
+++ 2018-Code/src/org/usfirst/frc/team2643/robot/Robot.java
@@ -146,7 +146,7 @@ public class Robot extends IterativeRobot {
 			ex: "position - left and switch - left" being cases 1-3, 
 			then "position - right and switch - left" being cases 4-6*/
 			switch(RobotMap.state) {
-					
+				
 			}
 		}
 	}
--- 2018-Code/src/org/usfirst/frc/team2643/robot/RobotMap.java
+++ 2018-Code/src/org/usfirst/frc/team2643/robot/RobotMap.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team2643.robot;
 
 public class RobotMap {
+	
+	
 	//boolean that tells if the motion to released the intake arms has been done
 	public static boolean armsReleased = false;
 	
@@ -8,10 +10,16 @@ public class RobotMap {
 	//1-left
 	//2-middle
 	//3-right
-	public static int position = 0;
+	public static int position = 0; //TODO
 	
 	//the autonomous program to run
 	//case 0: the robot just crosses the auto line 
 	//case 1: switch-left position-left
-	static int state = 0;
+	static int state = 0; //TODO before match starts
+	
+	//distance from the starting position to the auto line in encoder ticks
+	static int autoLineDistance = 0; //TODO
+	
+	//the speed for the robot during autonomous 
+	public static double autoSpeed = 0; //TODO
 }
