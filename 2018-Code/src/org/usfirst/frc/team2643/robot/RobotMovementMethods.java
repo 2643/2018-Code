0aeb9284b52440a619df1f93b703d45b6e7326eb
Sanjana679 <sanjana@nandi.net>
2018-01-22 17:14:48 -0800

    Made turning methods. Need to test before testing auto code.

--- 2018-Code/src/org/usfirst/frc/team2643/robot/RobotMovementMethods.java
+++ 2018-Code/src/org/usfirst/frc/team2643/robot/RobotMovementMethods.java
@@ -1,5 +1,22 @@
 package org.usfirst.frc.team2643.robot;
 
 public class RobotMovementMethods {
-
+	/*Notes: 
+	 * the wheels on the outside of the turn need to moving faster
+	 * than the wheels on the inside of the turn 
+	 */
+	
+	//TODO TEST THIS SEPERATELY BEFORE TESTING AUTO CODE!!!
+	//turns the robot right
+	public static void turnRight(){
+		Robot.setLeftMotors(0.4);
+		Robot.setRightMotors(-0.2);
+	}
+	
+	//TODO TEST THIS SEPERATELY BEFORE TESTING AUTO CODE!!!
+	//turns the robot left
+	public static void turnLeft(){
+		Robot.setRightMotors(0.4);
+		Robot.setLeftMotors(-0.2);
+	}
 }
