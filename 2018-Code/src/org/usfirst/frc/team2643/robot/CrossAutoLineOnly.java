be3fe4a667747e7fcafb6e3252686ea6a0daa422
Sanjana679 <sanjana@nandi.net>
2018-01-22 16:30:04 -0800

    Finished CrossAutoLineOnly.start()

--- 2018-Code/src/org/usfirst/frc/team2643/robot/CrossAutoLineOnly.java
+++ 2018-Code/src/org/usfirst/frc/team2643/robot/CrossAutoLineOnly.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team2643.robot;
 
 public class CrossAutoLineOnly {
-
+	public static void start(){
+		if(Robot.leftEncoder.get() < RobotMap.autoLineDistance && Robot.rightEncoder.get() < RobotMap.autoLineDistance){
+			Robot.setAll(RobotMap.autoSpeed);
+		}else{
+			Robot.setAll(0);
+		}
+	}
 }
