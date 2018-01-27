3e03b1cad3adcecee2546d52c5a7c07bef4165dc
team <team@team2643-Ubuntu>
2018-01-27 11:42:49 -0800

    deleted turn code, replaced with my own beautiful code

--- 2018-Code/src/org/usfirst/frc/team2643/robot/RobotMovementMethods.java
+++ 2018-Code/src/org/usfirst/frc/team2643/robot/RobotMovementMethods.java
@@ -33,18 +33,32 @@ public class RobotMovementMethods {
 		}
 		else
 		{
-			robotMap.set
+			finishTurn();
 		}
 		//if it hasnt reached its goal yet
-		if(!checkIfReachedGoal(RobotMap.righEncoder.get(), AutoState.rightEncoderGoal))
+		if(!checkIfReachedGoal(RobotMap.leftEncoder.get(), AutoState.leftEncoderGoal))
 		{
 			//set the motor in the correct direction
-			RobotMovementMethods.setRightMotors(
-					Utils.getSign(AutoState.rightEncoderGoal)*RobotMap.cruisingSpeed);
+			RobotMovementMethods.setLeftMotors(
+					Utils.getSign(AutoState.leftEncoderGoal)*RobotMap.cruisingSpeed);
+		}
+		else
+		{
+			finishTurn();
 		}
 	}
 
-	public static void 
+	public static void finishTurn()
+	{
+		AutoState.robotState = AutoState.NOTHING;
+		//stop all drive motors and reset everything
+		stopAll();
+		RobotMap.leftEncoder.reset();
+		RobotMap.rightEncoder.reset();
+
+		AutoState.leftEncoderGoal = 0;
+		AutoState.rightEncoderGoal = 0;
+	}
 
 	/**
 	 * Checks if the current encoder value has reached its goal
@@ -75,117 +89,7 @@ public class RobotMovementMethods {
 		return false;
 	}
 
-	//TODO TEST THIS SEPERATELY BEFORE TESTING AUTO CODE!!!
-	/**
-	 * turnRight()
-	 * 		makes the robot turn 90 degrees
-	 */ 
-	public static void turnRight(){
-
-		switch(RobotMap.turnRightState){
-		case 1:
-			//if debug is true, print debug statements
-			if(RobotMap.DEBUG)
-				System.out.println("First Case for turnRight() method");
-
-
-			//set all of the drive motors to zero
-			stopAll();
-
-			//reset the encoders before turning
-
-
-			//but it has not turned yet
-			RobotMap.finishedTurning = false;
-			RobotMap.turnRightState = 2;
-			break;
-		case 2:
-
-			//if the debug statements are set to true, print the debug statements
-			if(RobotMap.DEBUG)
-				System.out.println("Second case for turnRight() method");
-
-			//if the robot has not turned 90 degrees,
-			if(Math.abs(RobotMap.leftEncoder.get()) < RobotMap.ticksTo90 && Math.abs(RobotMap.rightEncoder.get()) < RobotMap.ticksTo90){
-
-				//set the left and right motors to opposite values so that it rotates
-				setLeftMotors(0.4);
-				setRightMotors(-0.4);
-
-			}else{
-
-				//once it is finished turning, set all the drive motors to zero
-				stopAll();
-
-				//reset the encoders after turning
-				RobotMap.leftEncoder.reset();
-				RobotMap.rightEncoder.reset();
-
-				RobotMap.turnRightState = 1;
-				//the robot has finished turning and stoppedBeforeTurning needs to be reset 
-				RobotMap.finishedTurning = true;
-			}
-			break;					
-		}
-	}
-
-	//TODO TEST THIS SEPERATELY BEFORE TESTING AUTO CODE!!!
-	/**
-	 * turnLeft() makes the robot turn left 90 degrees
-	 */
-	public static void turnLeft(){
-
-		switch(RobotMap.turnLeftState){
-		case 1: 
-
-			//if debug is set to true. then print the debug statements
-			if(RobotMap.DEBUG == true)
-				System.out.println("First Case for turnLeft() method");
-
-			//set all of the drive motors to zero
-			stopAll();
-
-			//reset the encoders before turning
-			RobotMap.leftEncoder.reset();
-			RobotMap.rightEncoder.reset();
-
-			//but it has not finished turning
-			RobotMap.finishedTurning = false;
-
-			RobotMap.turnLeftState = 2;
-
-			break;
-
-		case 2:
-
-			//if debug is true, print the debug statements
-			if(RobotMap.DEBUG == true)
-				System.out.println("Second Case for turnLeft() method");
-
-			//if the robot has not turned ninety degrees...
-			if(Math.abs(RobotMap.leftEncoder.get()) < RobotMap.ticksTo90 && Math.abs(-RobotMap.rightEncoder.get()) < RobotMap.ticksTo90){
-
-				//set the drive motors to opposite values to rotate the robot
-				setLeftMotors(-0.4);
-				setRightMotors(0.4);
-			}
-			else //else the robot has turned ninety degrees
-			{
-				//set the motors to zero after turning
-				stopAll();
-
-				//reset the encoders after turning
-				RobotMap.leftEncoder.reset();
-				RobotMap.rightEncoder.reset();
-
-				RobotMap.turnLeftState = 1;
-
-				//the robot has finished turning
-				RobotMap.finishedTurning = true;
-			}
-			break;
-		}
-	}
+	
 
 	public void liftToSwitchHeight() {
 
