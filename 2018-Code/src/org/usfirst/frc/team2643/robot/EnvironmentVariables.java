a06e40c9bd5065efac723cec060b95746f7a8538
Team2643 <team2643@gmail.com>
2018-01-31 15:51:53 -0800

    First commint

--- 2018-Code/src/org/usfirst/frc/team2643/robot/EnvironmentVariables.java
+++ 2018-Code/src/org/usfirst/frc/team2643/robot/EnvironmentVariables.java
@@ -12,16 +12,16 @@ public class EnvironmentVariables {
 	
 	
 	//distance from the starting position to the auto line in encoder ticks
-	public static final int autoLineDistance = 0; //TODO
+	public static final int autoLineDistance = 3703; //TODO
 
 	//encoder ticks it takes for the robot to turn 90 degrees
 	//to turn right, make this positive
 	//to turn left, make this negative
 	
-	public static final int ticksTo90 = 0; //TODO
+	public static final int ticksTo90 = 150; //TODO
 
 	//the distance in encoder ticks from the starting position to the middle of the field switch
-	public static final int ticksToMiddleOfSwitch = 0; //TODO
+	public static final int ticksToMiddleOfSwitch = 4000; //TODO
 	
 	//the distance in encoder ticks from the starting position to the fence facing the driver station
 	public static final int ticksToBeforeSwitch = 0; //TODO
@@ -30,10 +30,10 @@ public class EnvironmentVariables {
 	public static final int ticksToSwitchHeight = 0; //TODO	
 	
 	//encoder ticks for length of the switch the switch
-	public static final int ticksToPassSwitch = 0; //TODO
+	public static final int ticksToPassSwitch = 5000; //TODO
 
 	//encoder ticks for the width of the field switch
-	public static final int ticksWidthOfSwitch = 0; //TODO
+	public static final int ticksWidthOfSwitch = 4000; //TODO
 	
 	//encoder ticks for the length of the field switch
 	public static final int ticksLengthOfSwitch = 0;//TODO
